62800ae63b38b198f30f04add60006f2f562c168
Blackjack234 <112246988+Blackjack234@users.noreply.github.com>
2024-03-15 20:34:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // it will teat all js code as newer version
+
+
+// alert(3+3)  //you can not use alert in node env as is, we need some diff syntax to run it
+
+console.log(3
+    +3) // code readability should be high , this kind of code is not good
+
+
+    let name = "Anirban"
+    let age = 18
+    let islogIn = true
+
+
+    //number => 2 to the pwer 53 (max range)
+    // bigint(very big numbers)
+    // string
+    // boolean =>true/false
+    // null => standalone value
+    // undefined => no value assign
+    // symbol => for uniqueness
+
+
+
+    // object 
+
+
+    console.log(typeof null);//o/p object
+    console.log(typeof undefined);//o/p undefined  ( for interview questions )
