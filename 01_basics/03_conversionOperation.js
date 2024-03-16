eda432a16a7ebd271694a14d05ab65eeeadd887f
Blackjack234 <112246988+Blackjack234@users.noreply.github.com>
2024-03-16 18:46:27 +0000

    conversion oparetion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -44,5 +44,37 @@ let SomeNumber = 33
 
 let stringNumber = String(SomeNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/* *************************** oparations   ************************************* */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Anirban"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
