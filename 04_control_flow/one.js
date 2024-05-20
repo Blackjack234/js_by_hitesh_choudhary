a1eb2c9c674c4d881daa1a76f5cb88b3ce5abba9
Blackjack234 <112246988+Blackjack234@users.noreply.github.com>
2024-05-20 18:37:13 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+// if
+
+// const tempareture = 43
+// if(tempareture < 50){
+//  console.log(`temp is less`);
+// }else{
+// console.log(`temp is great`);
+// }
+// console.log("Execute");
+
+//comparision oparator
+
+// <, >, <= ,>= , == ,===,!=,!==
+
+// scope
+
+// const score = 200 
+
+// if(score >100){
+//  let power = "fly"
+//  console.log(`user power is ${power}`);
+// }else{
+//     console.log("no power");
+// }
+// console.log(`user power is ${power}`);
+
+
+// const balance = 1000 
+
+// if(balance < 500){
+//     console.log("less then 500");
+
+// }else if(balance <750){
+//  console.log("less then 750");
+
+// }else{
+//     console.log("less then 1200");
+// }
+
+
+// conditional rendaring
+
+const isloogedIn = true
+const useDabet = true
+const gmailLogIn = false
+
+if(isloogedIn && useDabet){
+  console.log("user can buy product");
+}
+
+
+if(isloogedIn || gmailLogIn){
+ console.log("user is looged in ");
+}
