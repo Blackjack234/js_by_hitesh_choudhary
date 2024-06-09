ba41658f9c8ca1710e9a39f7e3d48cda82ed99fc
Blackjack234 <112246988+Blackjack234@users.noreply.github.com>
2024-06-09 17:18:02 +0000

    do while loop

---
+++ 05_iterations/do-while.js
@@ -0,0 +1,23 @@
+// while loop
+let index = 0
+// while (index <= 10) {
+//      console.log(`value of index ${index}`);
+
+//      index = index+2
+// }
+
+
+let myArray = ['flash','batman','superman']
+
+// while (index<myArray.length) {
+//     console.log(`value is ${myArray[index]}`);
+//     index = index+1
+// }
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <=10);
+
+// in do-while loop condition is checked after do scope is run , so this loop will run atleast 1 time even if condition is not matched .
